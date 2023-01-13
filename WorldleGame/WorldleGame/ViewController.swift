b16c4b2ee5a7c5efa2eb2778945e8bb43f6f8999
Иван Лотохин <ivanlotohin@Ivans-MacBook-Air.local>
2023-01-13 20:37:26 +0200

    accidently choose swiftui instead of uikit, this is right initial commit

---
+++ WorldleGame/WorldleGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldleGame
+//
+//  Created by Иван Лотохин on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
