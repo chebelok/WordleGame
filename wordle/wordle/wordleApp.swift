d6173adf8124bc13d5cbd527f4997f9253c4e0bd
Иван Лотохин <ivanlotohin@Ivans-MacBook-Air.local>
2023-01-13 16:49:48 +0200

    initial commit

---
+++ wordle/wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by Иван Лотохин on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
