df76b51ac941018c565b46c062d313956ab9e679
Иван Лотохин <ivanlotohin@Ivans-MacBook-Air.local>
2023-01-14 22:46:38 +0200

    added cells for words

--- WorldleGame/WorldleGame/coreLogic/BoardViewController.swift
+++ WorldleGame/WorldleGame/coreLogic/BoardViewController.swift
@@ -1,21 +1,68 @@
 import UIKit
 
-class BoardViewController: UIViewController {
+class BoardViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    var guesses: [[Character?]] = Array(repeating: Array(repeating: nil, count: 5), count: 6)
 
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        let collectionVIew = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionVIew.translatesAutoresizingMaskIntoConstraints = false
+        collectionVIew.backgroundColor = .clear
+        collectionVIew.register(KeyCell.self, forCellWithReuseIdentifier: KeyCell.identifier)
+        return collectionVIew
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension BoardViewController {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return guesses.count
     }
-    */
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return guesses[section].count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCell.identifier, for: indexPath) as? KeyCell else {
+            fatalError()
+        }
+        
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width-margin)/5
+
+        return CGSize(width: size, height: size)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+
+        return UIEdgeInsets(
+            top: 2,
+            left: 1,
+            bottom: 2,
+            right: 1
+        )
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
 }
