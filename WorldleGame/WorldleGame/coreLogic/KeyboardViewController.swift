fe896c08b197a34d92dfd7cfe6fcb57f075f1a5f
Иван Лотохин <ivanlotohin@Ivans-MacBook-Air.local>
2023-01-14 03:10:37 +0200

    make correct placement for key cells

--- WorldleGame/WorldleGame/coreLogic/KeyboardViewController.swift
+++ WorldleGame/WorldleGame/coreLogic/KeyboardViewController.swift
@@ -47,8 +47,6 @@ extension KeyboardViewController {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCell.identifier, for: indexPath) as? KeyCell else {
             fatalError()
         }
-//        let letter = keys[indexPath.section][indexPath.row]
-//        cell.configure(with: letter)
         return cell
     }
 
@@ -60,19 +58,28 @@ extension KeyboardViewController {
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+
+        var left: CGFloat = 1
+        var right: CGFloat = 1
+
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width-margin)/10
+        let count: CGFloat = CGFloat(collectionView.numberOfItems(inSection: section))
+
+        let inset: CGFloat = (collectionView.frame.size.width - (size * count) - (2 * count))/2
+
+        left = inset
+        right = inset
+
         return UIEdgeInsets(
             top: 2,
-            left: 0,
+            left: left,
             bottom: 2,
-            right: 0
+            right: right
         )
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //
-//        collectionView.deselectItem(at: indexPath, animated: true)
-//        let letter = keys[indexPath.section][indexPath.row]
-//        delegate?.keyboardViewController(self,
-//                                         didTapKey: letter)
+
     }
 }
