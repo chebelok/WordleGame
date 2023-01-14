8e46fa82007e0a7be547fbe6a969485b546e1685
Иван Лотохин <ivanlotohin@Ivans-MacBook-Air.local>
2023-01-14 02:24:41 +0200

    added layout for keyboard

--- WorldleGame/WorldleGame/coreLogic/KeyboardViewController.swift
+++ WorldleGame/WorldleGame/coreLogic/KeyboardViewController.swift
@@ -1,11 +1,29 @@
 import UIKit
 
 class KeyboardViewController: UIViewController {
+    
+    let letters = ["qwertyuiop","asdfghjkl","zxcvbnm"]
+    private var keys: [[Character]] = []
 
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        let collectionVIew = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionVIew.translatesAutoresizingMaskIntoConstraints = false
+        collectionVIew.backgroundColor = .yellow
+        return collectionVIew
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
         
-        view.backgroundColor = .red
 
     }
     
