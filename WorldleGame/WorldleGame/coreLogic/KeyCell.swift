ce3b8a4e9fdd9d77bf4fbc7bc6cd00aec72935d8
Иван Лотохин <ivanlotohin@Ivans-MacBook-Air.local>
2023-01-14 19:30:09 +0200

    added lettes to key cells

--- WorldleGame/WorldleGame/coreLogic/KeyCell.swift
+++ WorldleGame/WorldleGame/coreLogic/KeyCell.swift
@@ -10,12 +10,38 @@ import UIKit
 class KeyCell: UICollectionViewCell {
     static let identifier = "KeyCell"
     
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemGray3
+        backgroundColor = .systemGray6
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
+
+    func configure(with letter: Character) {
+        label.text = String(letter).uppercased()
     }
 }
